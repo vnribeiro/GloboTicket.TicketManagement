1a834267115035d2f63a4cc786e931f9a56336aa
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-09-20 19:38:34 -0300

    Refactor method chaining for readability
    Reformatted the method chaining for `AddAutoMapper` and
    `AddValidatorsFromAssemblies` in the `AddApplicationServices`
    method. The method calls are now split across multiple lines
    to enhance readability.

--- src/GloboTicket.TicketManagement.Application/Extensions/ApplicationServicesRegistrationExtensions.cs
+++ src/GloboTicket.TicketManagement.Application/Extensions/ApplicationServicesRegistrationExtensions.cs
@@ -8,14 +8,16 @@ public static class ApplicationServicesRegistrationExtension
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         // Register AutoMapper Assemblies
-        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services
+            .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         // Register MediatR Assemblies
         services.AddMediatR(cfg => 
             cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
 
         // Register Fluent Validation Assemblies
-        services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+        services
+            .AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
